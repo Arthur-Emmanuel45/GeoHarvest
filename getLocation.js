98060813a4621ddaf9b7c2284e2a42b7e77e73d1
Arthur-Emmanuel45 <arthuremmanuel45@gmail.com>
2025-05-12 22:29:04 +0000

    location try

--- getLocation.js
+++ getLocation.js
@@ -1,11 +1,16 @@
 const errorMessage = document.getElementById("error_message");
 const getLat = document.getElementById("get_lat");
+const getLong = document.getElementById("get_long");
 if (!navigator.geolocation) {
     errorMessage.innerHTML = "no gelocation avaliable"
 }
 
 const success = (pos) => {
-    getLat.innerHTML = pos;
+    getLat.innerHTML = pos.coords.latitude;
+    getLong.innerHTML = pos.coords.longitude;
+}
+const error = () => {
+    console.log(err);
 }
 
-navigator.geolocation.getCurrentPosition(success, error, option);
+navigator.geolocation.watchPosition(success, error);
