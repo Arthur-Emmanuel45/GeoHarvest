a2953199e87d8054b1b2150b0544242a7bc9f532
Arthur-Emmanuel45 <arthuremmanuel45@gmail.com>
2025-05-13 17:48:32 +0000

    update on location

--- getLocation.js
+++ getLocation.js
@@ -9,9 +9,6 @@ const points = [];
 let polylineLayer = null;
 
 
-// let pendingPoint = null;
-// const confirmedPoints = [];
-
 //plot the map
 var map = L.map('map');
 
@@ -20,35 +17,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Click to select a point (but not confirm yet)
-// map.on("click", function (e) {
-// pendingPoint = e.latlng;
-// alert(`Selected: ${pendingPoint.lat.toFixed(5)}, ${pendingPoint.lng.toFixed(5)}\nClick "Confirm Point" to save.`);
-// });
-
-// Confirm the clicked point
-// document.getElementById("confirm_point").onclick = function () {
-// if (!pendingPoint) {
-//     alert("Click on the map first to select a point.");
-//     return;
-// }
-
-// confirmedPoints.push([pendingPoint.lat, pendingPoint.lng]);
-
-// L.marker(pendingPoint).addTo(map)
-//     .bindPopup("Confirmed Point")
-//     .openPopup();
-
-// const li = document.createElement("li");
-// li.textContent = `Lat: ${pendingPoint.lat.toFixed(5)}, Lng: ${pendingPoint.lng.toFixed(5)}`;
-// coordsList.appendChild(li);
-
-// L.polyline(confirmedPoints, { color: 'blue' }).addTo(map);
-
-
-// pendingPoint = null;
-// };
-//getting coordinate
 const getUserLocation = () => {
     if (!navigator.geolocation) {
         errorMessage.innerHTML = "no gelocation avaliable"
@@ -58,21 +26,29 @@ const getUserLocation = () => {
         const lat = pos.coords.latitude;
         const lng = pos.coords.longitude;
   
-        // confirmedPoints.push([lat, lng]);
         points.push([lat, lng]);
         L.marker([lat, lng]).addTo(map).bindPopup("Current Location").openPopup();
         getLat.innerHTML = pos.coords.latitude;
         getLong.innerHTML = pos.coords.longitude;
 
+
+        // Show coordinates in list
         const li = document.createElement("li");
         li.textContent = `Lat: ${lat.toFixed(5)}, Lng: ${lng.toFixed(5)}`;
         document.getElementById("coords_list").appendChild(li);
 
         map.setView([lat, lng], 13);
 
+        // Remove old polyline if it exists
+        if (polylineLayer) {
+            map.removeLayer(polylineLayer);
+        }
+        
         // Draw updated polyline
-        if (polylineLayer) map.removeLayer(polylineLayer);
-        polylineLayer = L.polyline(points, { color: 'blue' }).addTo(map);
+        polylineLayer = L.polyline(points, {
+            color: 'blue',
+            weight: 4
+        }).addTo(map);
     }
     const error = (err) => {
         if(err.code === 1){
